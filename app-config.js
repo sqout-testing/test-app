a248a5daada5b8dcd65e0f69bf65b10813a386c0
sqout-testing <immuu.au@gmail.com>
2026-04-24 18:08:26 -0400

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,15 @@
+window.AUDIENCE_SYNC_CONFIG = {
+  audienceApiKey: 'REPLACE_WITH_AUDIENCE_LABS_API_KEY',
+  audienceId: '690932ed-86d3-4348-9851-fdec475a1db9',
+  supabaseUrl: 'https://bbpnptsvwcjrdkrqjrce.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJicG5wdHN2d2NqcmRrcnFqcmNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNDM2MDEsImV4cCI6MjA5MjYxOTYwMX0.83aOOa2l5tzIbOaNKLyIbnN-k1tjtEZhxW35W83aFQo',
+  typeSuffix: 'hvac',
+  pageSize: 500,
+  audienceTimeoutMs: 180000,
+  retryWaitMs: 2000,
+  maxRetryWaitMs: 180000,
+  maxRetries: 20,
+  pageDelayMs: 1500,
+  minSkiptraceMatchScore: 5,
+  stateWhitelist: ['CA']
+};
